5b8e201d141b561b6a1ac7f6be3d92970448e356
ZhouZhengyi <121880399@qq.com>
2022-08-04 14:36:49 +0800

    Initial commit

---
+++ app/src/main/java/com/beem/algorithm/zzy/ZzyTest.java
@@ -0,0 +1,16 @@
+package com.beem.algorithm.zzy;
+
+/**
+ * ================================================
+ * 作    者：ZhouZhengyi
+ * 创建日期：2022/8/4 14:22
+ * 描    述：
+ * 修订历史：
+ * ================================================
+ */
+public class ZzyTest {
+
+    public static void main(String[] args){
+        System.out.println("test");
+    }
+}
